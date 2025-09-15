1e8a4ec6a7734dad29c6b0ffd6f326b2992fffe8
joberth <joberthpalomera18@gmail.com>
2025-09-15 08:34:52 +0800

    Added new Mage

--- hero.js
+++ hero.js
@@ -19,5 +19,18 @@ class Hero {
         }
     }
 
+    class Mage extends Hero {
+        constructor(name,health,attack,mana){
+            super(name,health,attack);
+            this.mana = mana;
+        }
+        useAbility(){
+            console.log(`${this.name} cast Fireball!`);
+        }
+    }
+
 const Warrior1 = new Warrior ("Thorin",100,10);
 Warrior1.useAbility();
+
+const mage = new Mage("Gandalf",80,8,50);
+mage.useAbility();
