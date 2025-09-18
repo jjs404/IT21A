a4cbfa855142e7aa9814a8a65bce6245ef19033e
joberth <joberthpalomera18@gmail.com>
2025-09-18 08:22:29 +0800

    Added Add basta mao na ohahay

--- hero.js
+++ hero.js
@@ -6,11 +6,35 @@ class Hero {
     this.name = name;
     this.#health = health;
     this.attack = attack;
+    this.Items = [];
     }
 
     getHealth() {
         return this.#health;
     }
+
+    getName() {
+        console.log(thisi.name);
+    }
+
+    getAttack() {
+        console.log(this.attack);
+    }
+
+    getStats() {
+        console.log("Name: " + this.name);
+        console.log("Health: " + this.health);
+        console.log("Attack: " + this.attack);
+    }
+
+    getItem() {
+        this.Items.push(item);
+    }
+
+    totalAttack() {
+        return this.attack + this.Items.reduce((sum,i)=>sum,i.bonusAttack,0);
+    }
+
 
 }
     class Warrior extends Hero {
@@ -29,8 +53,40 @@ class Hero {
         }
     }
 
-const Warrior1 = new Warrior ("Thorin",100,10);
-Warrior1.useAbility();
+    class Items {
+        constructor(name,bonusAttack) {
+            this.name = name;
+            this.bonusAttack = bonusAttack;
+        }
+    }
 
-const mage = new Mage("Gandalf",80,8,50);
-mage.useAbility();
+const sword = new Items("Sword",5);
+const staff = new Items("Staff",3);
+
+
+
+const Warrior1 = new Warrior ("Clark",100,10);
+Warrior1.getName();
+Warrior1.getAttack();
+Warrior1.getItem();
+console.log(Clark.totalAttack());
+
+// const mage = new Mage("Tion",80,8,50);
+// mage.useAbility();
+
+// const Gandalf = new Warrior ("Clark",100,10);
+// // Gandalf.useAbility();
+// Gandalf.getStats();
+
+function performAbility(Hero) {
+    console.log("\n");
+    Hero.useAbility
+} 
+
+
+function performAbility(Hero) {
+    Hero.useAbility();
+}
+
+performAbility(Warrior1);
+performAbility(Gandalf);
