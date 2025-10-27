ac14dd27e50e05ea356e152de15d44248473aa60
joberth <joberthpalomera18@gmail.com>
2025-10-27 09:22:34 +0800

    ayha pani na himo nga branch sir

---
+++ 1st Sem/hello.js
@@ -0,0 +1,18 @@
+console.log("Hello World");
+
+
+num1 = 2;
+num2 = 3;
+
+sum = num1 + num2;
+diff = num1 - num2;
+prod = num1 * num2;
+Quo = num1 / num2;
+console.log(sum);
+
+console.log("the sum of "+ num1 +" & " + num2 + "  is  " + sum)
+console.log("the Difference of "+ num1 +" & " + num2 + "  is  " + diff)
+console.log("the Product of "+ num1 +" & " + num2 + "  is  " + prod)
+console.log("the Quotient of "+ num1 +" & " + num2 + "  is  " + Quo)
+
+console.log ("This is the lab/factorial branch")
---
+++ 1st Sem/hero.js
@@ -0,0 +1,92 @@
+class Hero {
+    #health;
+
+    constructor (name,health,attack) {
+    
+    this.name = name;
+    this.#health = health;
+    this.attack = attack;
+    this.Items = [];
+    }
+
+    getHealth() {
+        return this.#health;
+    }
+
+    getName() {
+        console.log(thisi.name);
+    }
+
+    getAttack() {
+        console.log(this.attack);
+    }
+
+    getStats() {
+        console.log("Name: " + this.name);
+        console.log("Health: " + this.health);
+        console.log("Attack: " + this.attack);
+    }
+
+    getItem() {
+        this.Items.push(item);
+    }
+
+    totalAttack() {
+        return this.attack + this.Items.reduce((sum,i)=>sum,i.bonusAttack,0);
+    }
+
+
+}
+    class Warrior extends Hero {
+        useAbility(){
+            console.log(`${this.name} uses power Strike!`);
+        }
+    }
+
+    class Mage extends Hero {
+        constructor(name,health,attack,mana){
+            super(name,health,attack);
+            this.mana = mana;
+        }
+        useAbility(){
+            console.log(`${this.name} cast Fireball!`);
+        }
+    }
+
+    class Items {
+        constructor(name,bonusAttack) {
+            this.name = name;
+            this.bonusAttack = bonusAttack;
+        }
+    }
+
+const sword = new Items("Sword",5);
+const staff = new Items("Staff",3);
+
+
+
+const Warrior1 = new Warrior ("Clark",100,10);
+Warrior1.getName();
+Warrior1.getAttack();
+Warrior1.getItem();
+console.log(Clark.totalAttack());
+
+// const mage = new Mage("Tion",80,8,50);
+// mage.useAbility();
+
+const Gandalf = new Warrior ("Clark",100,10);
+Gandalf.useAbility();
+Gandalf.getStats();
+
+function performAbility(Hero) {
+    console.log("\n");
+    Hero.useAbility
+} 
+
+
+function performAbility(Hero) {
+    Hero.useAbility();
+}
+
+performAbility(Warrior1);
+performAbility(Gandalf);
